759d5d5300aa570b89a6d2bcfb9c4498552ace70
fabien pittiani <fabien.pittiani@gmail.com>
2024-03-06 22:27:27 +0100

    index en cours

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -1,2 +1,25 @@
 class DesksController < ApplicationController
+  def index
+    @desks = Desk.all
+    @appointment = Appointment.new
+
+    # génération des données pour l'envoyer au controller Stimulus au format json
+    # simulation des disponibilités;
+    data = []
+    @desks.each do |desk|
+      if rand < 0.3
+       data << {id: desk.id, name: desk.name, level: desk.level , dispo: true}
+      else
+         data << {id: desk.id, name: desk.name, level: desk.level , dispo: false}
+      end
+    end
+    puts data
+
+     respond_to do |format|
+      format.html
+      format.json { render json: data }
+    end
+  end
+
+
 end
---
+++ app/javascript/controllers/deskconnect_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="deskconnect"
+export default class extends Controller {
+  static targets = ['link', "modal"]
+  connect() {
+    const url = "/desks";
+
+    // demande des infos des bureaux aux Desks_controller#index en Json
+    fetch(url, {
+      headers: { "Accept": "application/json" },
+    })
+      .then(response => response.json())
+      .then((data) => {
+
+        // récupération des bureaux de l'étage issue de l'HTML
+        const desks_drawing = Array.from(this.linkTarget.querySelectorAll('rect[data-bureau]'));
+        desks_drawing.forEach((desk_drawing) => {
+          data.forEach((desk) => {
+            if (desk.name === desk_drawing.dataset.bureau) {
+              if (desk.dispo) {
+                desk_drawing.style.fill = "green"
+              } else {
+                desk_drawing.style.fill = "red"
+              }
+            }
+
+          })
+        })
+
+      })
+
+  }
+
+  form(e) {
+    this.modalTarget.style.display = "block"
+    console.log(e);
+
+
+  }
+
+
+}
