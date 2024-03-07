ad6f6e3ce01dbd867012a0097b174c7e4af06664
fabien pittiani <fabien.pittiani@gmail.com>
2024-03-07 17:05:27 +0100

    commentaires du controller desk

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -13,22 +13,23 @@ class DesksController < ApplicationController
     data = []
 
     @desks.each do |desk|
-      #récupération des paramètres (date d'arrivée et départ)
-         deskid = desk.name
+      #initialisation de l'heure actuelle
          current_time = Time.now
          time_rails = current_time.strftime("%Y-%m-%d %H:%M:%S UTC")
          time_rails_plus_une_heure = (current_time + 1.hour).strftime("%Y-%m-%d %H:%M:%S UTC")
-        if start_at
-         date_start = Date.parse(start_at) ||  time_rails
-         date_end = Date.parse(end_at) || time_rails_plus_une_heure
+        #récupération des paramètres, si il y en a
+         if start_at
+         date_start = Date.parse(start_at)
+         date_end = Date.parse(end_at)
+         #sinon récupération de l"heure en cours
         else
          date_start =  time_rails
          date_end = time_rails_plus_une_heure
         end
-
+        # recherche des dispos
          booked = Appointment.where("(? < end_at) AND (? > start_at) AND desk_id = ?", date_start, date_end, desk.name).exists?
 
-  # si date alors remplir le tableau data
+  # on remplit le tableau si il y a des créneaux ou non de trouvé
        if booked
         data << {id: desk.id, name: desk.name, level: desk.level, dispo: false}
        else
