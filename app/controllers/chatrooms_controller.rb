199c712c296b9132cd8272e10b935ed2de651803
Ingrid Gustave <147619421+IngridGustave@users.noreply.github.com>
2024-03-22 12:10:41 +0100

    correctifs front

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,5 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    @messages = @chatroom.messages.last(5)
   end
 end
