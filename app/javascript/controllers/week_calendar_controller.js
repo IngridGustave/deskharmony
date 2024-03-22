537e5927e41cfcb4c7f819f2b0c88c6eb4eacdbc
laura vidal <lauravidal.vl@gmail.com>
2024-03-22 15:39:41 +0100

    calendar next week

--- app/javascript/controllers/week_calendar_controller.js
+++ app/javascript/controllers/week_calendar_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="week-calendar"
 export default class extends Controller {
-  //static targets = ["dialog"]
+  static targets = ["date", "dialog"]
 
   connect() {
     console.log("connect")
-    //   this.dialogTarget.style.display = 'none';
+    this.dialogTarget.style.display = 'none';
   }
 
   open(event) {
@@ -20,19 +20,22 @@ export default class extends Controller {
     //  this.dialogTarget.style.display = 'none';
   }
 
-  nextWeek(e) {
-    e.preventDefault();
+  nextWeek(event) {
+    event.preventDefault();
     console.log("next week")
-    const url = "/desks/weekcalendar"
+    const dateParam = this.dateTarget.dataset.date
+    const url =  "/desks/weekcalendar?date=" + dateParam.replace(/"/g, '')
     fetch(url, {
       method: "GET",
       headers: { "Accept": "text/plain" },
     })
       .then(response => response.text())
       .then((data) => {
+        console.log(data);
         let weekCalendar = document.querySelector('.calendar-design.neon-effect');
         console.log(weekCalendar)
         weekCalendar.outerHTML = data;
       })
   }
+
 }
